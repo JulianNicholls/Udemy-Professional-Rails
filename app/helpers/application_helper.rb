4b7ae697dd9144ddca50b4c5be1276139048e136
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-22 19:24:10 +0100

    Added routes for Chefs and Recipes.
    Added #index for recipes.
    Added gravatar helper in application_helper.rb.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
-module ApplicationHelper
+module ApplicationHelper    # Available to all views
+  def gravatar_for(chef, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest chef.email
+    size        = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+
+    image_tag gravatar_url, alt: chef.name, class: 'gravatar'
+  end
 end
