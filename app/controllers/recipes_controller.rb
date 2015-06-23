f5a4135c2c9238dee481669312276eef82a6bdc9
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-23 11:48:29 +0100

    Added show action for a recipe.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,30 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def show
+    @recipe = Recipe.find params[:id]
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new recipe_params
+    @recipe.chef = Chef.find(2)
+
+    if @recipe.save
+      flash[:success] = "Your Recipe was Created Successfully"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 end
