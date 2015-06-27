6f32abda8663566f300f663e1e61784aa067f0b4
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-27 10:22:37 +0100

    Added review list, destroy action, and edit outline.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :require_user_or_admin, only: [:edit, :update, :destroy]
+
   def create
     @review = Review.new review_params
     @review.chef = current_user
@@ -12,9 +15,33 @@ class ReviewsController < ApplicationController
     redirect_to :back
   end
 
+  def edit
+  end
+
+  def update
+    @review.update review_params
+    binding.pry
+  end
+
+  def destroy
+    @review.destroy
+    flash[:success] = "The Review has been Deleted Successfully"
+  end
+
   private
 
     def review_params
       params.require(:review).permit :recipe_id, :rating, :body
+    end
+
+    def set_review
+      @review = Review.find params[:id]
+    end
+
+    def require_user_or_admin
+      return if current_user?(@review.chef) || current_user.admin?
+
+      flash[:danger] = "You can only update your own reviews"
+      redirect_to recipes_path
     end
 end
